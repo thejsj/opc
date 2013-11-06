27843e63c584c316fac52ae24153986263bc5085
thejsj <jorge.silva@thejsj.com>
2013-11-05 23:45:13 -0500

    Fixing Typo

--- wp-content/themes/OPC-TheLeader/functions.php
+++ wp-content/themes/OPC-TheLeader/functions.php
@@ -181,20 +181,20 @@ include(TEMPLATEPATH . '/leader_functions.php');
 		register_post_type( 'opc_communicado',
 			array(
 				'labels' => array(
-					'name' => __( 'Communicados' ), // general name for the post type, usually plural. The same as, and overridden by $post_type_object->label
-					'singular_name' => __( 'Communicado' ), // name for one object of this post type. Defaults to value of name
-					'menu_name' => __( 'Communicados' ), // the menu name text. This string is the name to give menu items. Defaults to value of name
-					'all_items' => __( 'Communicados' ), //  the all items text used in the menu. Default is the Name label
-					'add_new' => __( 'Add New Communicado' ), //  the add new text. The default is Add New for both hierarchical and non-hierarchical types. When internationalizing this string, please use a gettext context matching your post type. Example: _x('Add New', 'product');
-					'add_new_item' => __( 'Add New Communicado' ), // the add new item text. Default is Add New Post/Add New Page
-					'edit_item' => __( 'Edit Communicado' ), // the edit item text. Default is Edit Post/Edit Page
-					'new_item' => __( 'New Communicado' ), // the new item text. Default is New Post/New Page
-					'view_item' => __( 'View communicado' ), // the view item text. Default is View Post/View Page
-					'search_items' => __( 'Search Communicados' ), // the search items text. Default is Search Posts/Search Pages
-					'not_found' => __( 'No Communicados found' ), // the not found text. Default is No posts found/No pages found
-					'not_found_in_trash' => __( 'No Communicados Found in Trash' ), //  the not found in trash text. Default is No posts found in Trash/No pages found in Trash
+					'name' => __( 'Comunicados' ), // general name for the post type, usually plural. The same as, and overridden by $post_type_object->label
+					'singular_name' => __( 'Comunicados' ), // name for one object of this post type. Defaults to value of name
+					'menu_name' => __( 'Comunicados' ), // the menu name text. This string is the name to give menu items. Defaults to value of name
+					'all_items' => __( 'Comunicados' ), //  the all items text used in the menu. Default is the Name label
+					'add_new' => __( 'Add New Comunicado' ), //  the add new text. The default is Add New for both hierarchical and non-hierarchical types. When internationalizing this string, please use a gettext context matching your post type. Example: _x('Add New', 'product');
+					'add_new_item' => __( 'Add New Comunicado' ), // the add new item text. Default is Add New Post/Add New Page
+					'edit_item' => __( 'Edit Comunicado' ), // the edit item text. Default is Edit Post/Edit Page
+					'new_item' => __( 'New Comunicado' ), // the new item text. Default is New Post/New Page
+					'view_item' => __( 'View Comunicado' ), // the view item text. Default is View Post/View Page
+					'search_items' => __( 'Search Comunicado' ), // the search items text. Default is Search Posts/Search Pages
+					'not_found' => __( 'No Comunicados found' ), // the not found text. Default is No posts found/No pages found
+					'not_found_in_trash' => __( 'No Comunicados Found in Trash' ), //  the not found in trash text. Default is No posts found in Trash/No pages found in Trash
 				),
-			'description' => 'Communicados/Press Releases contain a title, text, a featuted image and a featured PDF',
+			'description' => 'Comunicados/Press Releases contain a title, text, a featuted image and a featured PDF',
 			'public' => true,
 			'hierarchical' => true,
 			'has_archive' => true,
@@ -652,7 +652,7 @@ include(TEMPLATEPATH . '/leader_functions.php');
 		register_setting( 'opc-settings-group', 'twitter_link' );
 		register_setting( 'opc-settings-group', 'youtube_link' );
 		register_setting( 'opc-settings-group', 'home_page_videos_title' );
-		register_setting( 'opc-settings-group', 'home_page_communicados_text' );
+		register_setting( 'opc-settings-group', 'home_page_comunicados_text' );
 		register_setting( 'opc-settings-group', 'home_page_sponsor_0' );
 		register_setting( 'opc-settings-group', 'home_page_sponsor_1' );
 		register_setting( 'opc-settings-group', 'home_page_sponsor_2' );
@@ -689,7 +689,7 @@ include(TEMPLATEPATH . '/leader_functions.php');
 		<table class="form-table">
 			<tr valign="top">
 			<th scope="row">Home Page Communicados Title ('NUESTROS ÚLTIMOS COMUNICADOS')</th>
-			<td><input type="text" name="home_page_communicados_text" value="<?php echo get_option('home_page_communicados_text'); ?>" /></td>
+			<td><input type="text" name="home_page_comunicados_text" value="<?php echo get_option('home_page_comunicados_text'); ?>" /></td>
 			</tr>
 			 
 			<tr valign="top">
